a6e5a78d5c3a5602bffef553352792d5bcaec04e
Kanhaiya Mishra <mkanhaiyas149@gmail.com>
2025-08-20 16:39:22 +0530

    Update firebaseConfig.js

--- frontend/src/config/firebaseConfig.js
+++ frontend/src/config/firebaseConfig.js
@@ -2,15 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_G8Z6CuxDfc2zgUr9kSU8V-cqkX30FeA",
-  authDomain: "esp32-19cea.firebaseapp.com",
-  databaseURL:
-    "https://esp32-19cea-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "esp32-19cea",
-  storageBucket: "esp32-19cea.firebasestorage.app",
-  messagingSenderId: "970460918317",
-  appId: "1:970460918317:web:6203094464ba3de234bea4",
-  measurementId: "G-QQVESWV4TS",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID" // optional, for analytics
 };
 
 const app = initializeApp(firebaseConfig);
